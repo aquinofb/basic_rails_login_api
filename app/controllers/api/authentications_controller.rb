2ab92aa40454d058e9c9e64931242db0f8507770
aquinofb <aquinofb@gmail.com>
2015-11-18 20:26:13 -0300

    Authentications: SignUp

--- app/controllers/api/authentications_controller.rb
+++ app/controllers/api/authentications_controller.rb
@@ -5,10 +5,23 @@ module API
       user = User.find_by(email: params[:email])
 
       if user && user.password == params[:password]
-        render json: {a: "b"}, status: :ok
+        render json: {done: true}, status: :ok
       else
-        render json: {e: "b"}, status: :not_found
+        render json: {errors: ["errors"]}, status: :not_found
       end
     end
+
+    def sign_up
+      user = User.new(user_params)
+      user.password = params[:password]
+      user.save!
+
+      render json: {done: true}, status: :ok
+    end
+
+    private
+      def user_params
+        params.require(:user).permit!(:email)
+      end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :authentications, only: [] do
       post :sign_in
+      post :sign_up
     end
   end
 end
