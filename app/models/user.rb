feb7d47ac2cdb0c57ab91e7a9490d31b47302eb1
aquinofb <aquinofb@gmail.com>
2015-11-18 20:09:00 -0300

    User with password using bcrypt

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  include BCrypt
+
+  def password
+    @password ||= Password.new(encrypted_password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.encrypted_password = @password
+  end
+end
