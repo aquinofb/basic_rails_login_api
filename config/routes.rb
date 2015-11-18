3bc80e24c677bfbda4639826d2fe0d83040c2fbc
aquinofb <aquinofb@gmail.com>
2015-11-18 20:34:08 -0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :authentications, only: [] do
-      post :sign_in
-      post :sign_up
+      collection do
+        post :sign_in
+        post :sign_up
+      end
     end
   end
 end
