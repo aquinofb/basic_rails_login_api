28fba1778a4417d988a6ab3081c8d167e327b5ce
aquinofb <aquinofb@gmail.com>
2015-11-18 21:19:58 -0300

    Authentication with token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  before_action :authenticate_with_token!
+
+  private
+    def auth_token
+    end
+
+    def current_user
+      @user
+    end
+
+    def authenticate_with_token!
+      @user = APIToken.find_by(token: auth_token).try(:user)
+      raise "Invalid token" if user.nil?
+    end
 end
